1eac11237bfa7d5ed16692ee0a5d17497ce8e73b
MiloYea <474810262@qq.com>
2025-10-02 21:32:41 -0600

    day1 two sum

---
+++ Day01_TwoSum/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] nums = { 2, 7, 11, 15 };
+        int target = 13;
+
+        int[] result = TwoSum(nums, target);
+
+        Console.WriteLine($"[{result[0]}, {result[1]}]");
+    }
+
+    static int[] TwoSum(int[] nums, int target)
+    {
+        Dictionary<int, int> map = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int complement = target - nums[i];
+
+            if (map.ContainsKey(complement))
+            {
+                return new int[] { map[complement], i };
+            }
+
+            if (!map.ContainsKey(nums[i]))
+            {
+                map.Add(nums[i], i);
+            }
+        }
+
+        throw new ArgumentException("No two sum solution");
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # LeetCode_CSharp
 My LeetCode solutions in C# with explanations
+
+
+#### 2025-10-02
+- 第一天进行了two-sun的编程
+- 使用的哈希算法，明天继续复习一下
