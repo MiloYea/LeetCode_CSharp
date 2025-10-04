4367a57f4936465e90f3f853d55debc5314bf336
MiloYea <474810262@qq.com>
2025-10-03 23:29:30 -0600

    day 2 add two number

---
+++ Day02_AddTwoNumber/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program {
+    static void Main(string[] args) {
+        var sol = new Solution();
+
+        Console.WriteLine(sol.IsAnagram("anagram", "nagaram")); // True
+        Console.WriteLine(sol.IsAnagram("rat", "car"));         // False
+    }
+}
+
+public class Solution {
+    public bool IsAnagram(string s, string t) {
+        if (s.Length != t.Length) return false;
+
+        var count = new int[26];
+        foreach (char c in s) count[c - 'a']++;
+        foreach (char c in t) count[c - 'a']--;
+
+        foreach (int val in count)
+            if (val != 0) return false;
+        return true;
+    }
+}
--- README.md
+++ README.md
@@ -5,3 +5,6 @@ My LeetCode solutions in C# with explanations
 #### 2025-10-02
 - 第一天进行了two-sun的编程
 - 使用的哈希算法，明天继续复习一下
+
+#### 2025-10-03
+- 求两个数目的和
